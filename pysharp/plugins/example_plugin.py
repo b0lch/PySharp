56e2012a803c58ddf1bb6c7b08e1e27c331f75e1
Bergaxolotl <bergaxolotl@gmail.com>
2026-08-31 07:05:18 +0200

    Update example_plugin.py

--- pysharp/plugins/example_plugin.py
+++ pysharp/plugins/example_plugin.py
@@ -1,8 +1,3 @@
-"""Example plugin showing how to extend PySharp translator.
-
-This demonstrates registering a new type name and a simple statement handler.
-Drop additional modules into `pysharp/plugins/` to add translation rules.
-"""
 from pysharp.extensions import register_type, register_statement
 
 
